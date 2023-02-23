80c04b1b60a86ff0b8edf0c80fd2dd8809d1820f
VorosDominik <medoxidhun@gmail.com>
2023-02-23 16:33:43 +0100

    Equalizer terheles

--- src/Metodosuk/Equalizer.java
+++ src/Metodosuk/Equalizer.java
@@ -9,21 +9,39 @@ import java.util.Random;
 
 public class Equalizer {
     public static void main(String[] args) {
+        elvegzett_feladat();
+    }
+
+    private static void elvegzett_feladat() {
         lilacsikok();
+        lilacsikok(19);
         lilacsikok();
-        lilacsikok();
-        lilacsikok();
+        lilacsikok(8);
         lilacsikok();
     }
 
     private static void lilacsikok() {
         Random rnd=new Random();
-        int db= rnd.nextInt(7)+1;
+        
+        int db= rnd.nextInt(1,7)+1;
         for (int i = 0; i < db; i++) {
             System.out.printf("\u001B[45m" + " ");
         
            
         }
+        
+        System.out.println("");
+    }
+    private static void lilacsikok(int a) {
+        Random rnd=new Random();
+        
+  
+        for (int i = 0; i < a; i++) {
+            System.out.printf("\u001B[45m" + " ");
+        
+           
+        }
+        
         System.out.println("");
     }
 }
