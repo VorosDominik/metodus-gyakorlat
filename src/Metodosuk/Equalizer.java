03fb898c54c779099f11275b909c6735deb46ae6
VorosDominik <medoxidhun@gmail.com>
2023-02-23 16:20:12 +0100

    Equalizer feladat

--- src/Metodosuk/Equalizer.java
+++ src/Metodosuk/Equalizer.java
@@ -4,9 +4,26 @@
  */
 package Metodosuk;
 
+import java.util.Random;
+
 
 public class Equalizer {
     public static void main(String[] args) {
+        lilacsikok();
+        lilacsikok();
+        lilacsikok();
+        lilacsikok();
+        lilacsikok();
+    }
+
+    private static void lilacsikok() {
+        Random rnd=new Random();
+        int db= rnd.nextInt(7)+1;
+        for (int i = 0; i < db; i++) {
+            System.out.printf("\u001B[45m" + " ");
         
+           
+        }
+        System.out.println("");
     }
 }
