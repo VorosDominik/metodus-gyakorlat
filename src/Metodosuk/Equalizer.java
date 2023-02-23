47e2585bc743499ec0b933585c39b48a534e17b9
VorosDominik <medoxidhun@gmail.com>
2023-02-23 16:42:57 +0100

    Equalizer terheles2

--- src/Metodosuk/Equalizer.java
+++ src/Metodosuk/Equalizer.java
@@ -14,34 +14,51 @@ public class Equalizer {
 
     private static void elvegzett_feladat() {
         lilacsikok();
-        lilacsikok(19);
+        lilacsikok(19 , false);
         lilacsikok();
-        lilacsikok(8);
+        lilacsikok(8, true);
         lilacsikok();
     }
 
     private static void lilacsikok() {
         Random rnd=new Random();
-        
-        int db= rnd.nextInt(1,7)+1;
-        for (int i = 0; i < db; i++) {
-            System.out.printf("\u001B[45m" + " ");
-        
-           
-        }
+        int db=rnd.nextInt(3,8);
+       lilacsikok(db);
         
         System.out.println("");
     }
-    private static void lilacsikok(int a) {
+    private static void lilacsikok(int db) {
         Random rnd=new Random();
         
   
-        for (int i = 0; i < a; i++) {
-            System.out.printf("\u001B[45m" + " ");
+        for (int i = 0; i < db; i++) {
+            szin();
         
            
         }
         
         System.out.println("");
     }
+
+    private static void szin() {
+        System.out.printf("\u001B[45m" + " ");
+    }
+     private static void lilacsikok(int db, boolean anyi) {
+        Random rnd=new Random();
+        
+         if (anyi==true) {
+             for (int i = 0; i < db; i++) {
+            szin();
+                 System.out.printf( "(" + db + ")" );
+         }
+           
+           
+        }
+         else{
+             lilacsikok();
+         }
+        
+        System.out.println("");
+    }
+
 }
