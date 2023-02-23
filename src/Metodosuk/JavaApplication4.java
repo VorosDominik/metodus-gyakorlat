2950f51d61db08b1a198a36d3cfe4912a131f137
VorosDominik <medoxidhun@gmail.com>
2023-02-23 16:01:27 +0100

    sok ccommit után

--- src/Metodosuk/JavaApplication4.java
+++ src/Metodosuk/JavaApplication4.java
@@ -14,10 +14,18 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        feladatok();
+    }
+
+    private static void feladatok() {
         elso10szam();
         kiir("az elsö 10 szám összege = " + elso10szam());
+        System.out.println("");
         osszead(10, 20);
         kiir("a és b öszege");
+        System.out.println("");
+        osszead(5, osszead(4, 6));
+        kiir("a három szam gyöke: " + gyökvonas(25));
     }
 
     private static int elso10szam() {
@@ -34,5 +42,19 @@ public class JavaApplication4 {
     }
     private static void kiir( String szoveg){
         System.out.printf(szoveg);
+    }
+    private static int osszead4(int a, int b, int c,int d){
+    int osszeg=a+b+c+d;
+        return osszeg;
+    
+    
+    
+    
+    
+    }
+    private static double gyökvonas(int gyök){
+        double vegeredmeny=  Math.sqrt(gyök);
+        return vegeredmeny;
+    
     }
 }
